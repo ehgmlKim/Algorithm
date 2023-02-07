8adea8fe59608593204a7ab89fa894c457d03fd8
김도희 <102472177+ehgmlKim@users.noreply.github.com>
2023-02-08 00:58:35 +0900

    [level 0] Title: 각도기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120829. 각도기/각도기.py
@@ -0,0 +1,10 @@
+def solution(angle):
+    answer = 0
+    if angle<90:
+            return 1
+    if angle==90:
+            return 2
+    if angle>90 and angle<180:
+            return 3
+    if angle==180:
+            return 4
