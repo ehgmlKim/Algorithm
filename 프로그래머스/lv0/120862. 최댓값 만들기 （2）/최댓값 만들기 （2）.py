9640cf5caa54231e7b76c73b5731bf126cece072
김도희 <102472177+ehgmlKim@users.noreply.github.com>
2023-02-10 23:02:29 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
@@ -0,0 +1,8 @@
+def solution(numbers):
+    answer = numbers[0]*numbers[1]
+    for i in numbers:
+        for j in numbers:
+            if i != j :
+                if i*j > answer :
+                    answer = i*j
+    return answer
