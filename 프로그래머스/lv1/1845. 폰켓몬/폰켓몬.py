34a4627fa693a5e3a2e009941739b0311a67b227
김도희 <102472177+ehgmlKim@users.noreply.github.com>
2023-02-12 16:42:49 +0900

    [level 1] Title: 폰켓몬, Time: 1.05 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
@@ -0,0 +1,10 @@
+import math as m
+def solution(nums):
+    answer = 0
+    if len(set(nums)) == len(nums)//2:
+        answer = len(nums)//2
+    elif len(set(nums)) > len(nums)//2:
+        answer = len(nums)//2
+    else :
+        answer = len(set(nums))
+    return answer
