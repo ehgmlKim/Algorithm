8022a5f95a84bf929fdf7f9e1cf99555c946abc3
김도희 <102472177+ehgmlKim@users.noreply.github.com>
2023-02-19 17:05:50 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.py
@@ -0,0 +1,17 @@
+from collections import deque
+
+def solution(progresses, speeds):
+    answer = []
+    days = []
+    
+    while len(progresses):
+        count = 0
+        while len(progresses) and progresses[0]>=100:
+            progresses.pop(0)
+            speeds.pop(0)
+            count += 1
+        progresses = [progresses[i]+speeds[i] for i in range(len(progresses))]
+        if count:
+            answer.append(count)
+    return answer
+            
