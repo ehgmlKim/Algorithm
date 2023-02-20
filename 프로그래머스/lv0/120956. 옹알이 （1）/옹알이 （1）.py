edbb6773dec7415071e77cde1c3770329c8cd552
김도희 <102472177+ehgmlKim@users.noreply.github.com>
2023-02-20 21:51:54 +0900

    [level 0] Title: 옹알이 (1), Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.py
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.py
@@ -1,17 +1,11 @@
 def solution(babbling):
     can = ["aya", "ye", "woo", "ma"]
-    answer = len(babbling)
-    dict = {}
+    answer = 0
+    
     for b in babbling:
-        if b not in dict:
-            dict[b] = 0
         for c in can:
-            if b.find(c)>-1:
-                dict[b] += 1
-    for b in range(len(babbling)):
-        for i in range(dict[babbling[b]]):
-            for c in can:
-                babbling[b] = babbling[b].split(c)[1] if babbling[b].startswith(c) else babbling[b]
-    
-    return len([i for i in babbling if len(i) == 0])
-    
+            if c in b:
+                b = b.replace(c, "*")
+        if len(b) == b.count("*"):
+            answer += 1
+    return answer
