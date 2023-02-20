7f539b95883fe7d50900989fae013dadcd9cc2cd
김도희 <102472177+ehgmlKim@users.noreply.github.com>
2023-02-21 03:20:01 +0900

    [Silver V] Title: 소수 찾기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1978. 소수 찾기/소수 찾기.py
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.py
@@ -4,10 +4,12 @@ arr =list(map(int, input().split()))
 answer = 0
 for a in arr:
   count = 0
-  for i in range(2, a):
-    if a%i==0:
-      n -= 1
-      break
   if a==1:
     n -= 1
+  else:
+    for i in range(2, a):
+      if a%i==0:
+        n -= 1
+        break
+  
 print(n)
