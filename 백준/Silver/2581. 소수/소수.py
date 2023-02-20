e2d2d33356423850f677f2fa1367409a336ddab7
김도희 <102472177+ehgmlKim@users.noreply.github.com>
2023-02-21 03:36:18 +0900

    [Silver V] Title: 소수, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2581. 소수/소수.py
+++ 백준/Silver/2581. 소수/소수.py
@@ -4,12 +4,12 @@ min_v = m
 sum = 0
 for i in range(n, m+1):
   cnt = 0
-  for j in range(2, int(i**(1/2))+1):
-    if i%j==0: 
-      cnt = 1
-      break
-  if not cnt:
-    if i != 1:
+  if i != 1:
+    for j in range(2, int(i**(1/2))+1):
+      if i%j==0: 
+        cnt = 1
+        break
+    if not cnt:
       sum += i
       min_v = min(min_v, i)
 if sum:
