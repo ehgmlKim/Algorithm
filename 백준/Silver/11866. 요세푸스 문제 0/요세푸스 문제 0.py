a0049f70cb5edd3a3334bd9b87a4e24f87cac9e4
김도희 <102472177+ehgmlKim@users.noreply.github.com>
2023-02-26 01:56:36 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
@@ -1,20 +1,11 @@
 n, k = map(int, input().split())
 arr = [i for i in range(1, n+1)]
-answer = []
 idx = k-1
 count = 1
-while True:
-  answer.append(arr[idx])
-  arr.remove(arr[idx])
-  if not len(arr):
-    break
+print("<", end='')
+for _ in range(n-1):
+  print(arr.pop(idx), end=", ")
   idx = idx+k-1
   if idx>=len(arr):
     idx = idx%len(arr)
-print('<', end='')
-for i in range(n):
-  if i == n-1:
-    print(answer[i], end=">")
-  else:
-    print(answer[i], end=', ')
-  
+print(arr[0], ">", sep="")
