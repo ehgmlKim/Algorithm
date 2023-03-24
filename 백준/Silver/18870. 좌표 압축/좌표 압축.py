7fc2d80646b622613cbbb213cd16c3a3c1529ee7
김도희 <102472177+ehgmlKim@users.noreply.github.com>
2023-03-24 23:15:47 +0900

    [Silver II] Title: 좌표 압축, Time: 1920 ms, Memory: 154804 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -1,6 +1,10 @@
-n=int(input())
-arr=list(map(int, input().split()))
-arr2 = sorted(list(set(arr)))
-dic = {arr2[i] : i for i in range(len(arr2))}
-for i in arr:
-    print(dic[i], end = ' ')
+import sys
+input = sys.stdin.readline
+n = int(input())
+arr = list(map(int, input().split()))
+set_arr = sorted(list(set(arr)))
+dict = {}
+for i,v in enumerate(set_arr):
+  dict[v] = i
+for a in arr:
+  print(dict[a], end=' ')
