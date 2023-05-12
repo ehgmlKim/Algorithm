8c65a26f9ad3795768ae7ac2ffeed7a1dd5e501e
김도희 <102472177+ehgmlKim@users.noreply.github.com>
2023-05-13 00:46:44 +0900

    Update 무지의 먹방 라이브.py

--- 프로그래머스/lv4/42891. 무지의 먹방 라이브/무지의 먹방 라이브.py
+++ 프로그래머스/lv4/42891. 무지의 먹방 라이브/무지의 먹방 라이브.py
@@ -1,25 +1,22 @@
+from heapq import heappush, heappop
 
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
 def solution(food_times, k):
-    if k>=sum(food_times):return -1
-    times_sorted = sorted([(idx, val) for idx, val in enumerate(food_times)], key=lambda x: x[1])
-    t = idx = 0
-    n = len(food_times)
-    cycle = times_sorted[0][1]
-    while k-(n*cycle)>0:
-        k -= n*cycle
-        n -= 1
-        idx += 1
-        cycle = times_sorted[idx][1]-times_sorted[idx-1][1]
-    return [i[0]+1 for i in sorted(times_sorted[idx:])][k%n]
+    # 방송 중단 전 음식을 다 먹는 경우
+    if sum(food_times) <= k:
+        return - 1
+    
+    foodHeap = []
+    length = len(food_times)    #남은 음식 개수
+    for i in range(length):
+        heappush(foodHeap, [food_times[i], i + 1]);
+    
+    time = 0
+    while (foodHeap[0][0] - time) * length < k:
+        k -= (foodHeap[0][0] - time) * length
+        time += (foodHeap[0][0] - time)
+        length -= 1
+        heappop(foodHeap)
+        
+    result = sorted(foodHeap, key = lambda x : x[1])    # index를 기준으로 heap을 다시 정렬
+    answer = result[k % length][1]
+    return answer
