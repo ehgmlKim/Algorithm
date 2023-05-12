1cf9d7bd89bf48e1a4fda3ba84f63579674d6c7c
김도희 <102472177+ehgmlKim@users.noreply.github.com>
2023-05-13 00:46:10 +0900

    Create 무지의 먹방 라이브2.py

---
+++ 프로그래머스/lv4/42891. 무지의 먹방 라이브/무지의 먹방 라이브2.py
@@ -0,0 +1,12 @@
+def solution(food_times, k):
+    if k>=sum(food_times):return -1
+    times_sorted = sorted([(idx, val) for idx, val in enumerate(food_times)], key=lambda x: x[1])
+    t = idx = 0
+    n = len(food_times)
+    cycle = times_sorted[0][1]
+    while k-(n*cycle)>0:
+        k -= n*cycle
+        n -= 1
+        idx += 1
+        cycle = times_sorted[idx][1]-times_sorted[idx-1][1]
+    return [i[0]+1 for i in sorted(times_sorted[idx:])][k%n]
