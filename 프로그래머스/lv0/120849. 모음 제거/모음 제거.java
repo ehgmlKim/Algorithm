7496c62bb33fe41ab86a17e913ecb2f617b0acac
김도희 <102472177+ehgmlKim@users.noreply.github.com>
2023-05-16 15:29:11 +0900

    [level 0] Title: 모음 제거, Time: 1.31 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        for(String s : my_string.split("")) {
+            if(s.equals("a") || s.equals("e") || s.equals("i") || s.equals("o") || s.equals("u"))
+                continue;
+            else
+                answer += s;
+        }
+        return answer;
+    }
+}
