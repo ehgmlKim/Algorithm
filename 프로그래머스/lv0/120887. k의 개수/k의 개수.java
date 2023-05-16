81a4cf3e0f4fc7fb4867dc11a8166a6b842ad976
김도희 <102472177+ehgmlKim@users.noreply.github.com>
2023-05-17 01:38:59 +0900

    [level 0] Title: k의 개수, Time: 1.97 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int i, int j, int k) {
+        int answer = 0;
+        for(int idx=i;idx<=j;idx++){
+            int num = idx;
+            while(num>0){
+                if(num%10==k)
+                    answer++;
+                num /= 10;
+            }
+        }
+        return answer;
+    }
+}
