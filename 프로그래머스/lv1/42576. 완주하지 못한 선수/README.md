6b867f3a5ec0f3f1201532392735228c9cee9da5
김도희 <102472177+ehgmlKim@users.noreply.github.com>
2023-07-06 22:38:25 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 47.59 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
 
 ### 성능 요약
 
-메모리: 95.3 MB, 시간: 339.10 ms
+메모리: 34 MB, 시간: 47.59 ms
 
 ### 구분
 
--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,13 +1,13 @@
 def solution(participant, completion):
-    dict={}
+    dict = {}
     for p in participant:
-        if p in dict:
-            dict[p] += 1
-        else:
+        if p not in dict:
             dict[p] = 1
+        else:
+            dict[p] += 1
     for c in completion:
         if c in dict:
             dict[c] -= 1
-    answer = {v:k for k,v in dict.items()}
-    return answer.get(1)
-
+    for k,v in dict.items():
+        if v!=0:
+            return k
