1c61e7c7218011d728ed6e36bde672c4896a450b
김도희 <102472177+ehgmlKim@users.noreply.github.com>
2023-07-06 23:59:55 +0900

    [level 2] Title: 전화번호 목록, Time: 105.64 ms, Memory: 28.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,7 +1,8 @@
-def solution(phone_book) :
+def solution(phone_book):
     answer = True
     phone_book.sort()
-    for i in range(len(phone_book)) :
-        if i+1<len(phone_book) and phone_book[i] == phone_book[i+1][:len(phone_book[i]):] :
+    l = len(phone_book)
+    for i in range(l-1):
+        if phone_book[i] == phone_book[i+1][:len(phone_book[i])]:
             return False
     return answer
