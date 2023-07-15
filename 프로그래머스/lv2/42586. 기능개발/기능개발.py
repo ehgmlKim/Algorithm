cb5a47c168a35c1f11e47b25b9f540fedcc2bf9b
김도희 <102472177+ehgmlKim@users.noreply.github.com>
2023-07-15 15:09:14 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.py
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.py
@@ -1,15 +1,19 @@
+from collections import deque
 def solution(progresses, speeds):
     answer = []
-    days = []
-    
-    while len(progresses):
-        count = 0
-        while len(progresses) and progresses[0]>=100:
-            progresses.pop(0)
-            speeds.pop(0)
-            count += 1
-        progresses = [progresses[i]+speeds[i] for i in range(len(progresses))]
-        if count:
-            answer.append(count)
+    days = deque()
+    for p, s in zip(progresses, speeds):
+        days.append((100-p)//s + (0 if (100-p)%s == 0 else 1))
+    print(days)
+    cnt = 1
+    d = days.popleft()
+    while len(days)>0:
+        if days[0] <= d:
+            days.popleft()
+            cnt += 1
+        else:
+            answer.append(cnt)
+            cnt = 0
+            d = days[0]
+    answer.append(cnt)
     return answer
-            
