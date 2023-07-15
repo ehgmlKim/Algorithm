cdc7cbad981a1fb0a79c01e76f2d3fb0c0bd6f4f
김도희 <102472177+ehgmlKim@users.noreply.github.com>
2023-07-15 12:39:26 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
@@ -1,14 +1,12 @@
-n, m=map(int, input().split())
-order = [i for i in range(1, n+1)]
+import sys
+input = sys.stdin.readline
+n, m = map(int, input().split())
+arr = [i for i in range(1, n+1)]
 for _ in range(m):
     i, j = map(int, input().split())
-    r = j - i + 1
-    for _ in range(r//2):
-        temp = order[i-1]
-        order[i-1] = order[j-1]
-        order[j-1] = temp
+    for _ in range((j-i+1)//2):
+        arr[i-1], arr[j-1] = arr[j-1], arr[i-1]
         i += 1
         j -= 1
-
-for o in order:
-    print(o, end=' ')
+for a in arr:
+    print(a, end=' ')
