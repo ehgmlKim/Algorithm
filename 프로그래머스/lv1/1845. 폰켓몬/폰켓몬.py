29536700b2c331a51f19da422ef5746f9be105e9
김도희 <102472177+ehgmlKim@users.noreply.github.com>
2023-07-15 14:34:56 +0900

    [level 1] Title: 폰켓몬, Time: 0.26 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
@@ -1,12 +1,7 @@
 def solution(nums):
     answer = 0
-    l = len(nums)
-    dic = {}
-    for n in nums:
-        if n not in dic:
-            dic[n] = 1
-        else:
-            dic[n] += 1
-    print(dic)
-    answer = min(l//2, len(dic))
+    n = len(nums)//2
+    dict = {}
+    arr = set(nums)
+    answer = min(n, len(arr))
     return answer
